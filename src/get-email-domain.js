e6370480f190d1d1e74e5be5fce267faaf14dd51
Valentina Nasanovich <valentina.nasanovich@gmail.com>
2023-04-13 13:17:52 +0300

    complete tasks count cats and get domain

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let domain;
+  for (let i =0; i< email.length;i++) {
+  if(email[i] === '@') {
+   domain = email.slice(i+1);
+      for (let j =0; j<domain.length; j++) {
+     if(domain[j] === '@') {
+         domain = domain.slice(j+1);
+  }
+}
+}
+  }
+  return domain;
 }
 
 module.exports = {
