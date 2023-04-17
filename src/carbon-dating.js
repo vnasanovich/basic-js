e0fa66f3a441effb366e7a00cc96cb16bdee5faa
Valentina Nasanovich <valentina.nasanovich@gmail.com>
2023-04-17 15:09:28 +0300

    complete task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+ if((+sampleActivity > MODERN_ACTIVITY || +sampleActivity <=0 || typeof sampleActivity !== 'string'|| isNaN(+sampleActivity)))  {
+  return false;}
+  else {
+    const age = Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693/5730);
+    return Math.ceil(age);
+  }
+
 }
 
 module.exports = {
